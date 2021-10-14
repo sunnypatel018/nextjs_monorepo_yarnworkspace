10ba733020234369be23d08bba184157348a3743
SUNNY PATEL <isunnypatel272@gmail.com>
2021-10-14 15:15:10 -0400

    rendering client side with data

--- packages/webapp/src/pages/csr.js
+++ packages/webapp/src/pages/csr.js
@@ -11,8 +11,54 @@
 // };
 
 // export default Csr;
-const SSR = typeof window === 'undefined';
 
-export default function index() {
-    return <div>{!SSR ? <p>Client side rendering</p> : null}</div>;
-}
+// ------------------------------------------------------
+
+// const SSR = typeof window === 'undefined';
+
+// export default function index() {
+//     return <div>{!SSR ? <p>Client side rendering</p> : <p>SSR</p>}</div>;
+// }
+
+// ------------------------------------------------------
+
+import { useState, useEffect } from 'react';
+
+const ToDoPage = () => {
+    const [toDos, setToDos] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/todos/')
+            .then((response) => response.json())
+            .then((data) => {
+                setToDos(data);
+                setIsLoading(false);
+            });
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading....</p>;
+    }
+    if (!toDos) {
+        return <p>No List to show to server side</p>;
+    }
+    return (
+        <div>
+            <h1>TODO List</h1>
+            <ul>
+                {toDos.map((toDo) => (
+                    <li key={toDo.id}>
+                        {toDo.title} -{' '}
+                        <span>
+                            ( {toDo.completed ? 'Completed' : 'Not Completed'} )
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ToDoPage;
